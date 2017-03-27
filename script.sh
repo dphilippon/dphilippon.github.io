95ea01f13ef89881efd5b947374d43d5b52c4d20
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-27 21:47:42 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,9 +9,12 @@ setup_git() {
 
 commit_website_files() {
   echo "Commiting GIT"
-  git checkout -b temp_pages
   git status 
+  echo "add -A"
+  
   git add -A
+  echo "commit "
+  
   git commit --message "Travis build #{Time.now.utc}"
   echo ${GH_TOKEN}
 }
@@ -19,10 +22,10 @@ commit_website_files() {
 upload_files() {
   echo "Uploading GIT"
   #git remote add origin https://${GH_TOKEN}@github.com/dphilippon/dphilippon.github.io.git
-  git push -f  --set-upstream origin master https://hqnghi88:$HQN_KEY@github.com/dphilippon/dphilippon.github.io.git
+  git push -f https://hqnghi88:$HQN_KEY@github.com/dphilippon/dphilippon.github.io.git
   #git push --quiet --set-upstream origin master
 }
 
 setup_git
 commit_website_files
-upload_files
+#upload_files
