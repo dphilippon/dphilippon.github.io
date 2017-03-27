d95eb75ecb04a97cf7dbc08b31f318c40592f6ff
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-27 21:03:10 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,7 +17,8 @@ commit_website_files() {
 upload_files() {
   echo "Uploading GIT"
   #git remote add origin https://${GH_TOKEN}@github.com/dphilippon/dphilippon.github.io.git
-  git push --quiet --set-upstream origin master
+  git push -f -q https://hqnghi88:$HQN_KEY@github.com/dphilippon/dphilippon.github.io.git
+  #git push --quiet --set-upstream origin master
 }
 
 setup_git
