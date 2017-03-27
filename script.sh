2a75e7781606778af552325ba1b0140a7022397b
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-27 23:51:34 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,15 +4,12 @@ setup_git() {
   echo "Setting up GIT"
   git config --global user.email "hqnghi88@gmail.com"
   git config --global user.name "hqnghi88"
-  git config --global push.default matching
-  git config user.name "hqnghi88@gmail.com"
-  git config user.password ${env.HQN_PWD}
+  git config --global user.password ${env.HQN_PWD}
 }
 
 commit_website_files() {
   echo "Commiting GIT"
   git add -A
-  echo "after add -A"
   echo "commit "
   
   git commit -q --message "Travis build #{Time.now.utc}"
@@ -22,9 +19,9 @@ commit_website_files() {
 upload_files() {
   echo "Uploading GIT"
   #git remote add origin https://${GH_TOKEN}@github.com/dphilippon/dphilippon.github.io.git
- # git push -f --set-upstream origin master https://hqnghi88:$HQN_KEY@github.com/dphilippon/dphilippon.github.io.git
+  git push -f  origin HEAD:master https://hqnghi88:$HQN_KEY@github.com/dphilippon/dphilippon.github.io.git
   
-  git push origin HEAD:master
+#  git push origin HEAD:master
   #git push --quiet --set-upstream origin master
 }
 
