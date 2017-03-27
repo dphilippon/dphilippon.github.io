312359c09f6a578d8fe77f602062ba9adc37241e
damienphilippon <damien.philippon.dev@gmail.com>
2017-03-27 17:41:45 +0700

    See Previous

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 setup_git() {
+  echo "Setting up GIT"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
+  echo "Commiting GIT"
   git checkout -b
   git add -A
   git commit --message "Travis build #{Time.now.utc}"
@@ -13,6 +15,7 @@ commit_website_files() {
 }
 
 upload_files() {
+  echo "Uploading GIT"
   git remote add origin https://${GH_TOKEN}@github.com/dphilippon/dphilippon.github.io.git
   git push --quiet --set-upstream origin master
 }
